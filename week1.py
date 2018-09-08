fb574dd9d8e26d777175169986e98a7c2ddebeb4
Shozaan <38714263+Shozaan@users.noreply.github.com>
2018-09-08 07:13:51 +0545

    Add files via upload

---
+++ week1.py
@@ -0,0 +1,14 @@
+class P:
+    def __init__(self):
+        self.inp = ""
+        
+    def input_s(self):
+        self.inp = input(" string ")
+      
+    def details(self):
+        x = self.inp[1::2]
+        print (x)
+        
+
+
+
